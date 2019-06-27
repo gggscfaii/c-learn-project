3f195e60cb38830687f0b584ac1aaf07d7b60bce
sdwang <544498438@qq.com>
2019-06-27 19:23:07 +0800

    calc.c

--- common-seventeen-project/calc.c
+++ common-seventeen-project/calc.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 typedef struct stack{
-    stack *next;
-    char *val;
+    struct stack *next;
+    char val;
 } stack;
 
 
-stack *push(stack *head, stack *e)
+stack *init(int n)
 {
+    stack *s = malloc(sizeof(stack));
+    s->next = NULL;
+    return s;
+}
+
+stack *push(stack *head, char c)
+{
+    stack *e = malloc(sizeof(stack));
+    e->val = c;
+
     if (head != NULL) {
         e->next = head;
     }
@@ -15,15 +27,38 @@ stack *push(stack *head, stack *e)
     return head;
 }
 
-stack *pop(stack *head)
+char pop(stack *head)
 {
     stack *e = head;
     head=head->next;
-    return e;
+    return e->val;
 }
 
 
 char *ptos(char *s)
 {
 
+}
+
+
+int main(int argc, const char *argv[])
+{
+    char *a = argv[1];
+    int n = strlen(a);
+
+    stack *s = init(n);
+    for (int i = 0; i < n; i++) {
+        if (a[i] == ')') {
+            printf("%c ",pop(s));
+        }
+        else if(a[i] == '+' || a[i] == '*'){
+            push(s, a[i]);
+        }
+        else if(a[i] >= '0' && a[i] <= '9'){
+            printf("%c ",a[i]);
+        }
+    }
+    printf("\n");
+
+    return 0;
 }
