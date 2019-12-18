231ee17c1a75c443870d0d1f16b7052f640c1283
sdwang <544498438@qq.com>
2019-12-18 17:27:40 +0000

    add client

--- chat/client.c
+++ chat/client.c
@@ -3,26 +3,54 @@
 
 #include <stdio.h>
 #include <unistd.h>
+#include <pthread.h>
 
 int fd;
 static void connect() {
-    chat *err;
+    char *err;
     if((fd = anetTcpConnect(err, "127.0.0.1", 6379)) == ANET_ERR){
         printf("connect server error.");
     }
+
+    if(anetNonBlock(err, fd) == ANET_ERR) {
+        printf("anet not block error");
+    }
 }
 
-static void readData() {
-   while(true) {
-       char buf
-       read(fd, __buf, __nbytes)
-   } 
+static void *readData(void *arg) {
+    int n;
+    char buf[1024];
+    while(1) {
+       if((n=read(fd, buf, 1024)) < 0) {
+            continue;    
+       }
+       write(STDOUT_FILENO, buf, sizeof(buf));
+   }
+   return NULL; 
 }
 
 int main(int argc, const char *argv[])
 {
+    pthread_t tid;
+    char buf[1024];
+    int n;
+
     connect();
-    
+    if(pthread_create(&tid, NULL, readData, NULL) != 0) {
+        printf("start read thread error");
+        close(fd);
+        return 0;
+    }
+
+    while(1) {
+        n = read(STDIN_FILENO, buf, 1024);
+        if(n < 0) {
+            printf("read stdin error");
+            continue;
+        }
+
+        write(fd, buf, n);
+    }
     return 0;
 }
 
