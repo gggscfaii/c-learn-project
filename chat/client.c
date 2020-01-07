032ce02005a3bfb70f9a8dceb428552e32a78c85
sdwang <544498438@qq.com>
2020-01-07 00:50:34 +0000

    add client.c

--- chat/client.c
+++ chat/client.c
@@ -1,22 +1,47 @@
 
 #include "anet.h"
+#include "ae.h"
 
 #include <stdio.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <errno.h>
+
+#define CHAT_CONNECTED 0x1
+#define CHAT_DISCONNECTING 0x2
+
+typedef struct chatAeContext {
+    int flags;
+} chatAeContext;
 
 typedef struct chatAeEvents {
+    chatAeContext *context;
     aeEventLoop *loop;
     int fd;
     int reading, writing;
 } chatAeEvents;
 
+
 static void chatAeReReadEvent(aeEventLoop *el, int fd, void *privdata, int mask) {
-    
+    char buf[1024*16];
+    int nread;
+
+    chatAeEvents *e = (chatAeEvents*)privdata;
+    nread = read(fd, buf, sizeof(buf));
+    if(nread <= 0) {
+        if(errno == EAGAIN) {
+            return;
+        }
+        e->context->flags |=  
+    }
 }
 
-static void chatAeWriteEvent(aeEventLoop *el, int fd, void *privdata, int mask) {
-    redisAeEvents *e = (redisAeEvents*)privdata;
+static void chatAeWriteEvent(aeeventloop *el, int fd, void *privdata, int mask) {
+    char buf[1024*16];
+    int nread;
+
+    chataeevents *e = (chataeevents*)privdata;
+    nread = read(fd, buf, sizeof(buf));
 
 }
 
@@ -25,20 +50,21 @@ static void chatAeAddRead(void *privData) {
    aeEventLoop *loop = e->loop;
    if (!e->reading) {
        e->reading = 1;
-       aeCreateFileEvent(loop, e->fd, AE_READABLE, cha)
+       aeCreateFileEvent(loop, e->fd, AE_READABLE, chatAeWriteEvent, e);
    }
 }
 
-static void chatAeAddWrite(void privdata) {
+static void chatAeAddWrite(void *privdata) {
     chatAeEvents *e = (chatAeEvents*)privdata;
     aeEventLoop *loop = e->loop;
     if(!e->writing) {
         e->writing = 1;
-        aeCreateFileEvent(loop, e->fd, AE_WRITABLE,)
+        aeCreateFileEvent(loop, e->fd, AE_WRITABLE, chatAeAddWrite, e);
     }
 }
+
 int fd;
-static void connect() {
+static void asyncConnect() {
     char *err;
     if((fd = anetTcpConnect(err, "127.0.0.1", 6379)) == ANET_ERR){
         printf("connect server error.");
